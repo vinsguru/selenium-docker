cfd0a2435c873ee28d3f37629a2d16b39a1a3d7b
vins <vino.infy@gmail.com>
2019-02-26 17:21:13 -0600

    script issue fix

--- healthcheck.sh
+++ healthcheck.sh
@@ -6,7 +6,8 @@
 
 echo "Checking if hub is ready - $HUB_HOST"
 
-while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]; do
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+do
 	sleep 1
 done
 
